de4c76f1a2d17605a6befd84681f167a9716be29
yamadapc <tacla.yamada@gmail.com>
2013-10-11 02:00:54 -0300

    Make middleware testing style more analogous to Scott's style

--- test/test_middleware.js
+++ test/test_middleware.js
@@ -11,14 +11,14 @@ describe('Middleware', function() {
         done();
       };
     }
-    describe('First array has same elements the second', function() {
-      it('should return false', test([0, 1, 2, 3, 4], [0, 2, 3, 1, 4], false));
+    describe('when first array has same elements the second', function() {
+      it('returns false', test([0, 1, 2, 3, 4], [0, 2, 3, 1, 4], false));
     });
-    describe('First array has all elements the second has, plus a few', function() {
-      it('should return false', test([0, 1, 2, 3, 4, 5], [0, 2, 1, 1, 4], false));
+    describe('when first array has all elements the second has, plus a few', function() {
+      it('returns false', test([0, 1, 2, 3, 4, 5], [0, 2, 1, 1, 4], false));
     });
-    describe('First array misses some of the elements of the second', function() {
-      it('should return true', test([0, 1, 2], [5, 1], true));
+    describe('when first array misses some of the elements of the second', function() {
+      it('returns true', test([0, 1, 2], [5, 1], true));
     });
   });
 });
